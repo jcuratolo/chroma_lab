62a2fae9ddd1c73974cc243711bdd272870839ea
jcuratolo <jcuratolo@me.com>
2014-10-30 20:44:35 -0700

    human driven breeding works

--- ga.js
+++ ga.js
@@ -1,18 +1,20 @@
 // Settings
-var GEN_POP = 5000; // size of each generation's population
+var GEN_POP = 9; // size of each generation's population
 var GEN_COUNT = 0; // begin generation counter at 0
-//var GOAL = [3,1,4,5,9,2,6,5,3,5]; // the goal, duh
-var GOAL = [ 0, 0, 0, 0, 0, 0, 0, 0, 0 ];
+var GENE_COUNT = 9; // how many genes each indiv has
+var GOAL = {};
 var MAX_CHANCE_DEATHS = 10; // randomly killed off each generation
-var MUTATE_CHANCE = .01 // Math.random() must be < to mutate
+var MUTATE_CHANCE = .25 // Math.random() must be < to mutate
 var MAX_GENS = 5000; // To cap off each run
-var ELITE_COUNT = 50 // How many best-ranked indivs to potentially clone
+var ELITE_COUNT = 1 // How many best-ranked indivs to potentially clone
 var ELITE_CLONE_CHANCE = 1; // Chance of fittest indivs cloning to next gen
 var SCORE_RECORD = [];
 var ELITE_RECORD = [];
 var CLONE_RECORD = [];
 var MUTATION_RECORD = [];
 
+var pop = [];
+initPop( pop );
 //===========================================
 
 evolve();
@@ -21,56 +23,81 @@ evolve();
 
 function evolve()
 {
-  var pop = [];
-  initPop( pop );
-  while ( !isGoalReached( pop ) && GEN_COUNT < MAX_GENS )
-  {
-    pop = iterateGenerations( pop );
-    GEN_COUNT++;
-    MUTATION_RECORD[ GEN_COUNT ] = 0;
-    CLONE_RECORD[ GEN_COUNT ] = 0;
-  }
-  for ( var i = 0; i < SCORE_RECORD.length; i++ )
-  {
-    document.getElementById( "candidates" ).innerHTML += "<li>" + ELITE_RECORD[ i ].join( "" ) + "</li>";
-  }
+  pop = iterateGenerations( pop );
+  writeGen( pop );
+  console.log( GOAL.genes );
 }
 
-function evolveNoLoop( paramCurrentGen )
+
+function writeGen( paramCurrentGen )
 {
-
+  // For each indiv
+  for ( var i = 0; i < GEN_POP; i++ )
+  {
+    document.getElementById( i ).style.background = getBG( paramCurrentGen[ i ] );
+    document.getElementById( i ).childNodes[ 1 ].style.color = getTitle( paramCurrentGen[ i ] );
+    document.getElementById( i ).childNodes[ 3 ].style.color = getText( paramCurrentGen[ i ] )
+    /*var fitnessScore = document.createElement( 'div' );
+    fitnessScore.innerHTML = paramCurrentGen[ i ].fitnessScore;
+    fitnessScore.style.background = "black";
+    fitnessScore.style.color = "white";
+    document.getElementById( i ).appendChild( fitnessScore );*/
+  }
 }
 
 
+function getBG( paramCurrentIndiv )
+{
+  return "rgb(" + paramCurrentIndiv.genes[ 0 ] + "," + paramCurrentIndiv.genes[ 1 ] + "," + paramCurrentIndiv.genes[ 2 ] + ")";
+}
 
-function initPop( pop )
+function getTitle( paramCurrentIndiv )
+{
+  return "rgb(" + paramCurrentIndiv.genes[ 3 ] + "," + paramCurrentIndiv.genes[ 4 ] + "," + paramCurrentIndiv.genes[ 5 ] + ")";
+}
+
+function getText( paramCurrentIndiv )
+{
+  return "rgb(" + paramCurrentIndiv.genes[ 6 ] + "," + paramCurrentIndiv.genes[ 7 ] + "," + paramCurrentIndiv.genes[ 8 ] + ")";
+}
+
+function improveFitness( paramCurrentIndiv )
+{
+  paramCurrentIndiv.fitnessScore = 0;
+  console.log( "improved fitness!" );
+  evolve();
+}
+
+function initPop( paramCurrentGen )
 {
   // For each individual
   for ( var ii = 0; ii < GEN_POP; ii++ )
   {
     // Push a new individual onto the population array
-    pop.push(
+    paramCurrentGen.push(
     {
       genes: [],
       fitness: [],
-      fitnessScore: 9999
+      fitnessScore: 5000
     } );
 
     // Loop through each gene of each individual
-    for ( var jj = 0; jj < GOAL.length; jj++ ) // for each gene
+    for ( var jj = 0; jj < GENE_COUNT; jj++ ) // for each gene
     {
       // randomly generate the genes
-      pop[ ii ].genes[ jj ] = Math.floor( Math.random() * 10 );
+      paramCurrentGen[ ii ].genes[ jj ] = Math.floor( Math.random() * 256 );
     }
     // Try uncommenting the following to see how giving a little help
     // changes the time to convergence
-    //pop[ ii ].genes[ 0 ] = 0;
-    //pop[ ii ].genes[ 1 ] = 0;
-    //pop[ii].genes[2] = 4;
-    //pop[ii].genes[3] = 5;
-    //pop[ii].genes[4] = 9;
+    //paramCurrentGen[ ii ].genes[ 0 ] = 0;
+    //paramCurrentGen[ ii ].genes[ 1 ] = 0;
+    //paramCurrentGen[ii].genes[2] = 4;
+    //paramCurrentGen[ii].genes[3] = 5;
+    //paramCurrentGen[ii].genes[4] = 9;
   } // end for loop for initial generation
-}
+  GOAL = paramCurrentGen[ 0 ]; // just to have something to begin with
+  console.log( paramCurrentGen );
+} // End intiPop()
 
 
 // Not currently used
@@ -87,6 +114,7 @@ function randomSmite( currentGenParam )
 
 function calcFitness( paramCurrentGen )
 {
+  // The fittest individual becomes the goal
   var total = 0;
   // for each individual in this generation
   for ( var i = 0; i < paramCurrentGen.length; i++ )
@@ -95,13 +123,13 @@ function calcFitness( paramCurrentGen )
     for ( var j = 0; j < paramCurrentGen[ i ].genes.length; j++ )
     {
       // calculate fitness
-      var temp = Math.abs( GOAL[ j ] - paramCurrentGen[ i ].genes[ j ] );
+      var temp = Math.abs( GOAL.genes[ j ] - paramCurrentGen[ i ].genes[ j ] );
       paramCurrentGen[ i ].fitness[ j ] = temp;
       // sum fitness of all genes
       total = total + paramCurrentGen[ i ].fitness[ j ];
     }
     // set fitnessScore from total
-    paramCurrentGen[ i ].fitnessScore = total;
+    paramCurrentGen[ i ].fitnessScore = total + 1;
     total = 0; // reset total
   } // end for each indiv
 }
@@ -130,6 +158,8 @@ function updateRecords( paramCurrentGen )
 function cloneElitesToNextGen( paramCurrentGen, paramNextGen )
 {
   // Clone elites to the next generation
+  // After sorting by fitnessScore, everything < ELITE_COUNT
+  // is considered an elite
   for ( var i = 0; i < ELITE_COUNT; i++ )
   {
     if ( Math.random() < ELITE_CLONE_CHANCE )
@@ -151,7 +181,7 @@ function mate( paramCurrentGen, paramNextGen )
     var child = {
       genes: [],
       fitness: [],
-      fitnessScore: 9999
+      fitnessScore: 5000
     };
     var firstParent = paramCurrentGen[ jj ];
     var secondParent = paramCurrentGen[ Math.floor( Math.random() *
@@ -165,6 +195,7 @@ function mate( paramCurrentGen, paramNextGen )
       .concat( secondParent.genes.slice( splitPoint ) );
     mutate( child );
     paramNextGen.push( child ); // add child to next generation
+    sortGenByFitness( paramNextGen );
   } // end for each indiv mating loop 
 }
 
@@ -173,8 +204,8 @@ function iterateGenerations( paramCurrentGen )
 {
   var nextGen = [];
 
-  calcFitness( paramCurrentGen );
   sortGenByFitness( paramCurrentGen );
+  calcFitness( paramCurrentGen );
   updateRecords( paramCurrentGen );
   cloneElitesToNextGen( paramCurrentGen, nextGen );
   mate( paramCurrentGen, nextGen );
@@ -212,8 +243,8 @@ function mutate( paramCurrentIndiv )
     return;
   }
   MUTATION_RECORD[ GEN_COUNT ] += 1;
-  // Randomly select gene and set to something betwee 0 and 9
+  // Randomly select gene and set to something betwee 0 and 255
   var geneIndex = Math.floor(
     Math.random() * paramCurrentIndiv.genes.length );
-  paramCurrentIndiv.genes[ geneIndex ] = Math.floor( Math.random() * 10 );
+  paramCurrentIndiv.genes[ geneIndex ] = Math.floor( Math.random() * 256 );
 }
